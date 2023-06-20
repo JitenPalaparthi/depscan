8505435b12c85966f65e7dd1916adbff5f61fea9
Jiten <jpalaparthi@gmail.com>
2023-06-21 00:43:20 +0530

    add skip directories and files functionality
    config.json file is used to define which directories and files to be skipped in order to process the scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,13 +1,21 @@
 package cmd
 
 import (
+	"errors"
+	"io/fs"
+	"log"
+	"path/filepath"
+
+	"github.com/JitenPalaparthi/depscan/config"
+	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
 
-var path string
-var format string
-var depth uint8
-var outFile string
+var (
+	path, format, outFile string
+	depth                 uint8
+	ErrSkipFile           = errors.New("skip file")
+)
 
 func init() {
 
@@ -24,5 +32,34 @@ var scanCmd = &cobra.Command{
 	Short: "scan scans a given repository",
 	Long:  "scan scans a given repository provided by given path",
 	Run: func(cmd *cobra.Command, args []string) {
+		fcount, dcount := 0, 0
+		glog.Infoln("Current path is ", path)
+		cnfg, err := config.New()
+		log.Println(cnfg, err)
+		filepath.WalkDir(path, func(p string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				if isElementExist(cnfg.IgnoreDirs, d.Name()) {
+					return filepath.SkipDir
+				}
+				dcount++
+			} else {
+				if isElementExist(cnfg.IgnoreFiles, d.Name()) {
+					return ErrSkipFile
+				}
+				fcount++
+			}
+			log.Println(p)
+			return nil
+		})
+
 	},
+}
+
+func isElementExist(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/JitenPalaparthi/depscan
 
 go 1.20
 
-require github.com/spf13/cobra v1.7.0
-
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/golang/glog v1.1.1
+	github.com/spf13/cobra v1.7.0
+	github.com/spf13/pflag v1.0.5
 )
+
+require github.com/inconshreveable/mousetrap v1.1.0 // indirect

68392d5f198e797f9edb2bd4b61aa7689928e7c4
Jiten <jpalaparthi@gmail.com>
2023-06-22 23:08:26 +0530

    add gradle and maven implementation

--- implement/test/implement_test.go
+++ implement/test/implement_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/JitenPalaparthi/depscan/implement"
 )
 
-func TestXxx(t *testing.T) {
+func TestPip(t *testing.T) {
 	pip := new(implement.Pip)
 	pip.FilePath = "requirements.txt"
 	gdeps, err := pip.Scan()
@@ -16,6 +16,16 @@ func TestXxx(t *testing.T) {
 		t.Fail()
 	}
 	if len(gdeps) != 11 {
+		t.Fail()
+	}
+}
+
+func TestGradle(t *testing.T) {
+	gradle := new(implement.Gradle)
+	gradle.FilePath = "build.gradle"
+	gdeps, err := gradle.Scan()
+	fmt.Println(gdeps, err)
+	if err != nil {
 		t.Fail()
 	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import (
-	"github.com/JitenPalaparthi/depscan/cmd"
-)
+import "github.com/JitenPalaparthi/depscan/cmd"
 
 func main() {
 	cmd.Execute()
+	// gradle := new(implement.Gradle)
+	// gradle.FilePath = "implement/test/build.gradle"
+	// gdeps, err := gradle.Scan()
+	// fmt.Println(gdeps, err)
+
+	// maven := new(implement.Maven)
+	// maven.FilePath = "implement/test/pom.xml"
+	// gdeps, err := maven.Scan()
+	// fmt.Println(gdeps, err)
 }
 
 //path := "/home/jiten/workspace/projects/depscan/test_repos/python/eLearning"
+//path := /home/jiten/workspace/projects/depscan/test_repos/gradle/zuul

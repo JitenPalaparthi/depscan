1c3554975d3be9358c2fdcfc1d09ec03074a2de5
Jiten <jpalaparthi@gmail.com>
2023-07-05 22:47:16 +0530

    update which information

--- cmd/which.go
+++ cmd/which.go
@@ -13,8 +13,8 @@ func init() {
 var whichCmd = &cobra.Command{
 	Use:   "which",
 	Short: "currently it supports Java,Python,JavaScript",
-	Long:  `currently it depscan supports Java(maven|gradle),Python(pip),JavaScript(npm->package-lock.json version 1 and 2)`,
+	Long:  `currently it depscan supports Java(maven|gradle),Python(pip),JavaScript(npm->package-lock.json version 1,2 and 3)`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(`currently it depscan supports Java(maven|gradle),Python(pip),JavaScript(npm->package-lock.json version 1 and 2)`)
+		fmt.Println(`currently it depscan supports Java(maven|gradle),Python(pip),JavaScript(npm->package-lock.json version 1,2 and 3)`)
 	},
 }

55e7d8ca6fb19a136ed6d8bb2bc219292bc3a6e3
Jiten Palaparthi <43363461+JitenPalaparthi@users.noreply.github.com>
2023-07-19 13:34:54 +0530

    Revert "Revert "Direct dependencies""

---
+++ implement/errors.go
@@ -0,0 +1,16 @@
+package implement
+
+import "errors"
+
+var (
+	ErrNilConfig               = errors.New("nil config")
+	ErrEmptyPath               = errors.New("empty path")
+	ErrInvalidOutfile          = errors.New("invalid outfile")
+	ErrInvalidOutfileFormat    = errors.New("invalid outfile format.It must be json | yaml | yml")
+	ErrNewImplement            = errors.New("use New function to create Implement object")
+	ErrNoDataToGenerateOutfile = errors.New("no data to generate output file")
+	ErrPathDoesNotExist        = errors.New("path does not exist")
+	ErrUnsupportedNPMVersion   = errors.New("unsupported npm version.Currently it supports only lockfileVersion-1,2 and 3 only.Old formats are not supported")
+	ErrUnsupportedFile         = errors.New("unsupported file")
+	ErrNoPackageFileFound      = errors.New("no package dependency file found")
+)
--- implement/pip/pip.go
+++ implement/pip/pip.go
@@ -1,4 +1,4 @@
-package implement
+package pip
 
 import (
 	"bufio"

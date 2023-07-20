1f3d5792c9333ee1079cbc3a0978cebe800d4a43
Jiten <jpalaparthi@gmail.com>
2023-07-20 16:12:14 +0530

    add dependencies embedded for each dependency
    fix node_module entry
    
    fix errors in direct dependencies

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -5,9 +5,10 @@ type Scanner interface {
 }
 
 type Dep struct {
-	Name    string `json:"name" yaml:"name"`
-	Version string `json:"version" yaml:"version"`
-	Direct  bool   `json:"direct" yaml:"direct"`
-	Source  string `json:"source" yaml:"source"`
-	Type    string `json:"type" yaml:"type"`
+	Name         string         `json:"name" yaml:"name"`
+	Version      string         `json:"version" yaml:"version"`
+	Direct       bool           `json:"direct" yaml:"direct"`
+	Source       string         `json:"source" yaml:"source"`
+	Type         string         `json:"type" yaml:"type"`
+	Dependencies map[string]any `json:"dependencies,omitempty" yaml:"dependencies,omitempty"`
 }
